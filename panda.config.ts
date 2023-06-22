63ab6603b66e65b295fe9ac3ff7cb2a2ee290cfb
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-06-22 09:52:37 -0300

    updatedc

--- panda.config.ts
+++ panda.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from '@pandacss/dev'
  
 export default defineConfig({
+  outExtension: "js",
   preflight: true,
   include: ['./imports/ui/**/*.{ts,tsx,js,jsx}', './client/**/*.{ts,tsx,js,jsx}', ],
   exclude: [],
-  outExtension: "js",
   outdir: 'styled-system'
 })
